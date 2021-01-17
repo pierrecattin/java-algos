9dc7fd2f9f060a861ee115143129223068f360a8
Pierre Cattin <cattin.pierre@gmail.com>
2021-01-17 12:05:33 +0100

    Graph class

--- src/com/pierrecattin/algorithms/utilities/Graph.java
+++ src/com/pierrecattin/algorithms/utilities/Graph.java
@@ -3,38 +3,45 @@ package com.pierrecattin.algorithms.utilities;
 import java.util.LinkedList;
 
 public class Graph {
-	int nbVertex; 
-	boolean directed;
-	private LinkedList<Integer> list[];
+	private int nbVertex; 
+	private boolean directed;
+	private LinkedList<Integer> edges[]; // each vertex has its LinkedList, containing the vertices it's connected to
 	
 	public Graph(int nbVertex, boolean directed) {
 		this.nbVertex=nbVertex;
 		this.directed = directed;
 		
-		list = new LinkedList[nbVertex];
+		edges = new LinkedList[nbVertex];
 		
 		for(int i=0; i<nbVertex; i++) {
-			list[i] = new LinkedList<>();
+			edges[i] = new LinkedList<>();
 		}
 	}
 	
 	public void addEdge(int source, int destination) {
-		list[source].addFirst(destination);
+		edges[source].addFirst(destination);
 		
-		if(directed) {
-			list[destination].addFirst(source);
+		if(!directed) {
+			edges[destination].addFirst(source);
 		}
 	}
 	
-	public void print() {
+	public String toString() {
+		String out ="";
+		if(directed) {
+			out += "Directed graph";
+		} else {
+			out += "Undirected graph";
+		}
+		out += " with " + nbVertex + " vertices";
 		for (int i=0; i<nbVertex; i++) {
-			System.out.println("Vertex "+i+" is connected to:");
+			out += "\n";
+			out += "Vertex "+i + " is connected to: ";
 			
-			for(int j=0; j<list[i].size(); j++) {
-				System.out.println(list[i].get(j)+" ");	
+			for(int j=0; j<edges[i].size(); j++) {
+				out += edges[i].get(j)+"; ";	
 			}
 		}
-		System.out.println("\n");
+		return(out);
 	}
-
 }
---
+++ src/com/pierrecattin/algorithms/utilities/GraphTester.java
@@ -0,0 +1,11 @@
+package com.pierrecattin.algorithms.utilities;
+
+public class GraphTester {
+	public static void main(String[] args) {
+		Graph graph=new Graph(5, false);
+		graph.addEdge(0, 2);
+		graph.addEdge(0,3);
+		graph.addEdge(3, 4);
+		System.out.println(graph);
+	}
+}
