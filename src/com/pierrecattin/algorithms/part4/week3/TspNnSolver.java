1d0ccf003ce84baf7f2ac6bc4f44a864a2fa8f12
Pierre Cattin <cattin.pierre@gmail.com>
2021-04-19 10:17:30 +0200

    make TspNn faster

--- src/com/pierrecattin/algorithms/part4/week3/TspNnSolver.java
+++ src/com/pierrecattin/algorithms/part4/week3/TspNnSolver.java
@@ -1,50 +1,56 @@
 package com.pierrecattin.algorithms.part4.week3;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class TspNnSolver {
 	private City[] cities;
 	private double tourDistance;	
-	ArrayList<Integer> visitedCities;
+	ArrayList<Integer> tour;
+	HashSet<Integer> unvisitedCities;
 	
 	TspNnSolver (City[] cities){
 		this.cities=cities;
 		tourDistance=0;
-		visitedCities=new ArrayList<>(0);	
+		tour=new ArrayList<>(0);	
+		unvisitedCities = new HashSet<>();
+		for(int i=0; i<cities.length; i++) {
+			unvisitedCities.add(i);
+		}
 	}
 	
 	public double solve() {
-		visitedCities.add(0);
-		while(visitedCities.size()<cities.length) {
-			System.out.println(100*visitedCities.size()/cities.length+"%");
+		tour.add(0);
+		unvisitedCities.remove(0);
+		while(unvisitedCities.size()>0) {
+			System.out.println(100*tour.size()/cities.length+"%");
 			visitClosest();
 		}
-		City firstCity = cities[visitedCities.get(0)];
-		City lastCity = cities[visitedCities.get(visitedCities.size()-1)];
+		City firstCity = cities[tour.get(0)];
+		City lastCity = cities[tour.get(tour.size()-1)];
 		tourDistance += lastCity.distanceFrom(firstCity);
 		return(tourDistance);
 	}
 	
 	
 	private void visitClosest() {
-		City currentCity = cities[visitedCities.get(visitedCities.size()-1)];
+		City currentCity = cities[tour.get(tour.size()-1)];
 		int closestIndex = -1;
 		double closestDist=Double.MAX_VALUE;
-		for (int i=0; i<cities.length; i++) {
-			if(!visitedCities.contains(i)) {
-				double currentDist = currentCity.distanceFrom(cities[i]);
-				if(currentDist<closestDist) {
-					closestIndex=i;
-					closestDist=currentDist;
-				}
+		for(int cityIndex:unvisitedCities) {
+			double currentDist = currentCity.distanceFrom(cities[cityIndex]);
+			if(currentDist<closestDist) {
+				closestIndex=cityIndex;
+				closestDist=currentDist;
 			}
 		}
-		visitedCities.add(closestIndex);
+		unvisitedCities.remove(closestIndex);
+		tour.add(closestIndex);
 		tourDistance+=closestDist;
 	}
 	
 	public ArrayList<Integer> getTour(){
-		return(visitedCities);
+		return(tour);
 	}
 	
 	public double getTourDistance() {
